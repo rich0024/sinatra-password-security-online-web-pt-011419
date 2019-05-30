ef906355fc10dc67e21174aeb1458ba0fa35ac1d
Richard Yance <yance0022@gmail.com>
2019-05-30 04:45:25 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+	post "/signup" do
+  	user = User.new(:username => params[:username], :password => params[:password]) 
+  	if user.save
+    	redirect "/login"
+  	else
+    	redirect "/failure"
+  	end
+	end
 	
 end
